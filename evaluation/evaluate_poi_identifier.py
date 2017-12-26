8a11857c2c186106bc0c3bc0b56c8b4b245d664e
Anton Rubisov <anton.rubisov@c3iot.com>
2017-12-25 20:04:41 -0800

    precision recall accuracy lesson

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -26,6 +26,47 @@ labels, features = targetFeatureSplit(data)
 
 
 
-### your code goes here 
+### your code goes here
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
+from sklearn.tree import DecisionTreeClassifier
 
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+
+# predict POIs within the test set only
+labels_pred = clf.predict(features_test)
+print "# of people in test set: {}".format(len(features_test))
+print "# of POIs predicted: {}".format(sum(labels_pred))
+
+from sklearn.metrics import confusion_matrix
+CM = confusion_matrix(labels_test, labels_pred)
+TN = CM[0][0]
+FN = CM[1][0]
+TP = CM[1][1]
+FP = CM[0][1]
+print "# of true positives: {}".format(TP)
+# precision: of all the ones you predicted to be of class X, what ratio was correct?
+print "precision: {}".format(TP*1.0/(TP+FP))
+# recall: of all the ones that ARE of class X, what ratio did you successfully predict?
+print "recall: {}".format(TP*1.0/(TP+FN))
+
+# from sklearn.metrics import accuracy_score
+# print accuracy_score(labels_test, labels_pred)
+# print clf.score(features_test, labels_test)
+
+### Bullshit Udacity round:
+print '######'
+print 'The bullshit udacity round'
+print '######'
+y_pred = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+y_true = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+
+CM = confusion_matrix(y_true, y_pred)
+TN = CM[0][0]
+FN = CM[1][0]
+TP = CM[1][1]
+FP = CM[0][1]
+print "precision: {}".format(TP*1.0/(TP+FP))
+print "recall: {}".format(TP*1.0/(TP+FN))
