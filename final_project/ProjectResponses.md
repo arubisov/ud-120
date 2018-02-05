d2ea631d464038c33e525d0dc4db4b0b0654b316
Anton Rubisov <anton.rubisov@c3iot.com>
2018-02-04 20:49:18 -0800

    p7 submission

---
+++ final_project/ProjectResponses.md
@@ -0,0 +1,73 @@
+# Identify Fraud from Enron E-mail
+2018.02.04  
+Anton Rubisov
+
+### Included in Folder
+- my_dataset.pkl
+- my_classifier.pkl
+- my_feature_list.pkl
+- poi_id.py
+- this markdown file
+
+### Question Responses
+1. Summarize for us the goal of this project and how machine learning is useful in trying to accomplish it. As part of your answer, give some background on the dataset and how it can be used to answer the project question. Were there any outliers in the data when you got it, and how did you handle those?  [relevant rubric items: “data exploration”, “outlier investigation”]
+
+  Our goal is to predict whether a given person in the Enron fraud scandal is a person-of-interest (POI). Machine learning is useful toward achieving this goal because it so happens that the Enron financial and e-mail dataset was made available publicly, and our hypothesis is that the data included within may have sufficient predictive power to produce a machine learning classifier, trained on hand-crafted labels, which is quicker to develop and delivers better results than what we can do by manually do by combing through the data - namely someone like myself with no education in forensic accounting!
+
+  The dataset contains 146 persons and 21 features. The dataset is incomplete, as there are many missing values (represented as 'NaN'), and many values that are zero where the value is unlikely to be zero in reality, such as a salary. Of the 3066 total values in the dataset, 160 are zeros, and 1358 are NaNs. In my analysis, all NaNs are converted to zeros, and any person for whom all my values of interest are equal to zero is removed from the dataset. Otherwise, all other zero values are retained.
+
+1. What features did you end up using in your POI identifier, and what selection process did you use to pick them? Did you have to do any scaling? Why or why not? As part of the assignment, you should attempt to engineer your own feature that does not come ready-made in the dataset -- explain what feature you tried to make, and the rationale behind it. (You do not necessarily have to use it in the final analysis, only engineer and test it.) In your feature selection step, if you used an algorithm like a decision tree, please also give the feature importances of the features that you use, and if you used an automated feature selection function like SelectKBest, please report the feature scores and reasons for your choice of parameter values.  [relevant rubric items: “create new features”, “intelligently select features”, “properly scale features”]
+
+  I began by including every numeric feature that was available in the financial features and e-mail features - which was everything except for the person's e-mail address. I didn't think it made sense to do feature engineering or outlier detection with the data in dictionary format, so the first thing I did was rewrite the featureFormat() function to return a pandas DataFrame, with the features as column names, and the person's name as the index. I was pleasantly surprised to find that it was a very small dataset - only 145 persons, and 20 features. This meant I would have very short run times for classifier training, and would be able to use GridSearchCV without worrying about that runtime being any more than a few seconds.
+
+  I knew I would be trying logistic regression and decision trees as starting algorithms, choosing both for their explanatory power, so I was not concerned about feature scaling since both algorithms are insensitive to feature scale. Outliers, however, were an issue. From the coursework itself, I already knew that Kenneth Lay had an astronomically large salary and bonus, so I created a new feature equal to bonus divided by salary, thinking that this might be a better indicator than either feature alone. I also plotted a boxplot of all the features. Rather unsurprisingly, total_stock_value and eercised_stock_options had the widest tails - the company executives were at the upper extremes of both distributions. Not knowing whether the data is corrupted or not, I made the decision to run the analysis with both outliers included - since, in fact, Kenneth Lay *did* earn that salary and bonus - and excluded, in case the data is messy and includes mistakes. For exclusion, I removed any rows that contained values which exceeded three standard deviations of the column to which they belong. It turned out excluding outliers produced a categorically better classifier.
+
+  I only used feature importance after the fact, not during feature selection. I found that the top features were:  
+  Feature[6] bonus: 40.53  
+  Feature[14] restricted_stock: -204.38  
+  Feature[17] from_poi_to_this_person: -14.45  
+
+1. What algorithm did you end up using? What other one(s) did you try? How did model performance differ between algorithms?  [relevant rubric item: “pick an algorithm”]
+
+  I wanted to try both a decision tree and logistic regression, as both offer the ability to understand feature importance. With the same underlying set of data, logistic regression performed substantially better than a decision tree, beating it on every single evaluation metric:  
+
+Metric | Logistic Regression | Decision Tree
+  ------ | ------------------- | -------------
+  Accuracy | 0.82457 |  0.71800
+  Precision | 0.41926 | 0.23547
+  Recall | 0.59200 | 0.43350
+  F1 | 0.49088 | 0.30517
+  F2 | 0.54693 | 0.37108
+  TP | 1184 | 867
+  FP | 1640 | 2815
+  FN | 816 | 1133
+  TN | 10360 | 9185
+
+  Notably, the logistic regression classifier gave me an area under the curve of 0.97 for the train and test set, so I was confident I had a decent classifier.
+
+3. What does it mean to tune the parameters of an algorithm, and what can happen if you don’t do this well?  How did you tune the parameters of your particular algorithm? What parameters did you tune? (Some algorithms do not have parameters that you need to tune -- if this is the case for the one you picked, identify and briefly explain how you would have done it for the model that was not your final choice or a different model that does utilize parameter tuning, e.g. a decision tree classifier).  [relevant rubric items: “discuss parameter tuning”, “tune the algorithm”]
+
+  Tuning parameters is important because classifiers behave very differently based on the parameters used to instantiate them - such as, for example, the maximum depth of a decision tree. I used GridSearchCV to iterate through values of C and penalty for logistic regression, as well as min_samples_split and max_depth for decision tree.
+
+1. What is validation, and what’s a classic mistake you can make if you do it wrong? How did you validate your analysis?  [relevant rubric items: “discuss validation”, “validation strategy”]
+
+  A classic mistake with machine learning is training and evaluating a model on the same dataset. It's a mistake because it runs the risk of overfitting to the training data, and not generalizing to new, unseen data. Validation refers to evaluating a trained classifier against a set of data which was not used for training. I achieved this rather simply by creating a train/test split of my data, using 70% for training and reserving 30% for training. As part of my tuning of the machine learning pipeline, I also made use of the tester.py code, which used 1000 cross-validation folds.
+
+1. Give at least 2 evaluation metrics and your average performance for each of them.  Explain an interpretation of your metrics that says something human-understandable about your algorithm’s performance. [relevant rubric item: “usage of evaluation metrics”]
+
+  I specifically tracked accuracy, precision, and recall while running my analysis. Here are my interpretations of those metrics in the context of this problem:
+
+  **Accuracy**: Of all the persons, what percentage am I labelling correctly?
+
+  **Precision**: Of all the persons I predict to be POIs, what percentage are actually POIs?
+
+  **Recall**: Of all the persons who actually are POIs, what percentage did I successfully predict as being POIs?
+
+
+### References
+-  https://stackoverflow.com/questions/23199796/detect-and-exclude-outliers-in-pandas-dataframe
+- https://stackoverflow.com/questions/34052115/how-to-find-the-importance-of-the-features-for-a-logistic-regression-model
+
+
+### Solemn Attestation / Declaration of Independence
+I hereby confirm that this submission is my work. I have cited above the origins of any parts of the submission that were taken from Websites, books, forums, blog posts, github repositories, etc.
--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -5,25 +5,231 @@ import pickle
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
-from tester import dump_classifier_and_data
+from tester import test_classifier, dump_classifier_and_data
+
+import numpy as np
+import pandas as pd
+from scipy import stats
+
+from time import time
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+# conda install -c conda-forge imbalanced-learn
+# from imblearn.over_sampling import SMOTE
+# from imblearn.pipeline import Pipeline
+from sklearn.metrics import roc_curve, auc, confusion_matrix, fbeta_score, make_scorer
+
+
+def featureFormatDF(dictionary, features, remove_NaN=True, remove_all_zeroes=True, remove_any_zeroes=False, sort_keys = False):
+    """ convert dictionary to numpy array of features
+        remove_NaN = True will convert "NaN" string to 0.0
+        remove_all_zeroes = True will omit any data points for which
+            all the features you seek are 0.0
+        remove_any_zeroes = True will omit any data points for which
+            any of the features you seek are 0.0
+        sort_keys = True sorts keys by alphabetical order. Setting the value as
+            a string opens the corresponding pickle file with a preset key
+            order (this is used for Python 3 compatibility, and sort_keys
+            should be left as False for the course mini-projects).
+        NOTE: first feature is assumed to be 'poi' and is not checked for
+            removal for zero or missing values.
+    """
+
+
+    return_list = []
+    keys_list = []
+
+    # Key order - first branch is for Python 3 compatibility on mini-projects,
+    # second branch is for compatibility on final project.
+    if isinstance(sort_keys, str):
+        import pickle
+        keys = pickle.load(open(sort_keys, "rb"))
+    elif sort_keys:
+        keys = sorted(dictionary.keys())
+    else:
+        keys = dictionary.keys()
+
+    for key in keys:
+        tmp_list = []
+        for feature in features:
+            try:
+                dictionary[key][feature]
+            except KeyError:
+                print "error: key ", feature, " not present"
+                return
+            value = dictionary[key][feature]
+            if value=="NaN" and remove_NaN:
+                value = 0
+            tmp_list.append( float(value) )
+
+        # Logic for deciding whether or not to add the data point.
+        append = True
+        # exclude 'poi' class as criteria.
+        if features[0] == 'poi':
+            test_list = tmp_list[1:]
+        else:
+            test_list = tmp_list
+        ### if all features are zero and you want to remove
+        ### data points that are all zero, do that here
+        if remove_all_zeroes:
+            append = False
+            for item in test_list:
+                if item != 0 and item != "NaN":
+                    append = True
+                    break
+        ### if any features for a given data point are zero
+        ### and you want to remove data points with any zeroes,
+        ### handle that here
+        if remove_any_zeroes:
+            if 0 in test_list or "NaN" in test_list:
+                append = False
+        ### Append the data point if flagged for addition.
+        if append:
+            return_list.append( np.array(tmp_list) )
+            keys_list.append(key)
+
+    df = pd.DataFrame(np.array(return_list))
+    df['names'] = keys_list
+    df.set_index('names', drop=True, inplace=True, verify_integrity=True)
+    df.columns = features
+
+    return df
+
+def run_logistic_regression_classifier(X_train, X_test, y_train, y_test, beta=1):
+    print "{} ({:.2f}%) positive labels in training set".format(len(y_train[y_train == True]), 100.0*len(y_train[y_train == True])/len(y_train))
+    print "{} ({:.2f}%) positive labels in test set".format(len(y_test[y_test == True]), 100.0*len(y_test[y_test == True])/len(y_test))
+
+    t0 = time()
+    param_grid = {
+             'C': [1e1, 1e2, 1e3, 1e4, 1e5, 1e6],
+              'penalty': ['l1', 'l2'],
+              }
+    f5_scorer = make_scorer(fbeta_score, beta=beta)
+    clf = GridSearchCV(estimator=LogisticRegression(class_weight='balanced'),
+                       param_grid=param_grid,
+                       scoring=f5_scorer)
+    clf = clf.fit(X_train, y_train)
+    print "training time: {}s".format(round(time() - t0, 3))
+    print "Best estimator found by grid search:"
+    print clf.best_estimator_
+
+    y_pred = clf.predict(X_test)
+    print "{} ({:.2f}%) positive labels in predictions".format(len(y_pred[y_pred == True]), 100.0*len(y_pred[y_pred == True])/len(y_pred))
+
+    conf = confusion_matrix(y_test, y_pred, labels=None, sample_weight=None)
+    print conf
+    TN, FP, FN, TP = conf.ravel()
+    print "TN: {}, FP: {}, FN: {}, TP: {}".format(TN, FP, FN, TP)
+
+    # accuracy: what proportion was correctly predicted out of total population?
+    print "accuracy: {}".format((TP+TN)*1.0/(TN+FP+FN+TP))
+    # precision: of all the ones you predicted to be of class X, what ratio was correct?
+    print "precision: {}".format(TP*1.0/(TP+FP))
+    # recall: of all the ones that ARE of class X, what ratio did you successfully predict?
+    print "recall: {}".format(TP*1.0/(TP+FN))
+
+    return clf.best_estimator_
+
+
+def run_decision_tree_classifier(X_train, X_test, y_train, y_test, beta=5):
+    print "{} ({:.2f}%) positive labels in training set".format(len(y_train[y_train == True]), 100.0*len(y_train[y_train == True])/len(y_train))
+    print "{} ({:.2f}%) positive labels in test set".format(len(y_test[y_test == True]), 100.0*len(y_test[y_test == True])/len(y_test))
+
+    t0 = time()
+    param_grid = {
+             'min_samples_split': [2,5,10,15,20],
+             'max_depth': range(1,5,1),
+              }
+    f5_scorer = make_scorer(fbeta_score, beta=beta)
+    clf = GridSearchCV(estimator=DecisionTreeClassifier(class_weight='balanced'),
+                       param_grid=param_grid,
+                       scoring=f5_scorer)
+    clf = clf.fit(X_train, y_train)
+    print "training time: {}s".format(round(time() - t0, 3))
+    print "Best estimator found by grid search:"
+    print clf.best_estimator_
+
+    y_pred = clf.predict(X_test)
+    print "{} ({:.2f}%) positive labels in predictions".format(len(y_pred[y_pred == True]), 100.0*len(y_pred[y_pred == True])/len(y_pred))
+
+    conf = confusion_matrix(y_test, y_pred, labels=None, sample_weight=None)
+    print conf
+    TN, FP, FN, TP = conf.ravel()
+    print "TN: {}, FP: {}, FN: {}, TP: {}".format(TN, FP, FN, TP)
+
+    # accuracy: what proportion was correctly predicted out of total population?
+    print "accuracy: {}".format((TP+TN)*1.0/(TN+FP+FN+TP))
+    # precision: of all the ones you predicted to be of class X, what ratio was correct?
+    print "precision: {}".format(TP*1.0/(TP+FP))
+    # recall: of all the ones that ARE of class X, what ratio did you successfully predict?
+    print "recall: {}".format(TP*1.0/(TP+FN))
+
+    return clf.best_estimator_
+
+
+def plot_roc(clf, X_test, y_test, X_train=None, y_train=None):
+    y_score = clf.decision_function(X_test)
+
+    # Compute ROC curve and ROC area for each class
+    fpr, tpr, _ = roc_curve(y_test, y_score)
+    roc_auc = auc(fpr, tpr)
+
+    plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange',
+             lw=lw, label='Test ROC curve (area = %0.2f)' % roc_auc)
+
+    if X_train is not None and y_train is not None:
+        y_score_train = clf.decision_function(X_train)
+
+        # Compute ROC curve and ROC area for each class
+        fpr_train, tpr_train, _ = roc_curve(y_train, y_score_train)
+        roc_auc_train = auc(fpr, tpr)
+
+        plt.plot(fpr_train, tpr_train, color='aqua',
+                 lw=lw, label='Train ROC curve (area = %0.2f)' % roc_auc_train)
+
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC for Logistic Regression Model')
+    plt.legend(loc="lower right")
+    plt.show()
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
+financial_features = ['salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses', 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 'director_fees']
+email_features = ['to_messages', 'from_poi_to_this_person', 'from_messages', 'from_this_person_to_poi', 'shared_receipt_with_poi']
 features_list = ['poi','salary'] # You will need to use more features
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
-### Task 2: Remove outliers
 ### Task 3: Create new feature(s)
+for k,v in data_dict.items():
+    if v['bonus'] != 'NaN' and v['bonus'] != 'NaN':
+        data_dict[k]['bonus_to_salary_ratio'] = 1.0 * v['bonus'] / v['salary']
+    else:
+        data_dict[k]['bonus_to_salary_ratio'] = 'NaN'
+
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
 
+### Task 1: Select what features you'll use.
+### features_list is a list of strings, each of which is a feature name.
+### The first feature must be "poi".
 ### Extract features and labels from dataset for local testing
-data = featureFormat(my_dataset, features_list, sort_keys = True)
-labels, features = targetFeatureSplit(data)
+features_list = ['poi'] + ['bonus_to_salary_ratio'] + financial_features + email_features
+df = featureFormatDF(my_dataset, features_list, sort_keys = True)
+
+### Task 2: Remove outliers
+df = df[(np.abs(stats.zscore(df)) < 3).all(axis=1)]
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -32,24 +238,37 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+X = df[features_list[1:]]
+y = df[features_list[0]]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+clf = run_logistic_regression_classifier(X_train, X_test, y_train, y_test)
+# plot_roc(clf, X_test, y_test, X_train, y_train)
+
+dt = run_decision_tree_classifier(X_train, X_test, y_train, y_test)
+# test_classifier(dt, my_dict, features_list)
+
+### Task 5: Tune your classifier to achieve better than .3 precision and recall
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
 ### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
+### stratified shuffle split cross validation. For more info:
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
+feature_weights = (df[features_list[1:]].std().as_matrix() * clf.coef_)[0]
+for idx, value in enumerate(feature_weights):
+    if abs(value) > 10:
+        print "Feature[{}] {}: {:.2f}".format(idx, features_list[idx], value)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
+
+my_dataset = df.to_dict(orient="index")
+
+test_classifier(dt, my_dataset, features_list)
+test_classifier(clf, my_dataset, features_list)
 
 dump_classifier_and_data(clf, my_dataset, features_list)
